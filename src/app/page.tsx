2aa9f348f79a652db346216d51f8c0fee0336db3
bigoli-seiya <nikake7bigoli@gmail.com>
2025-07-14 15:12:07 +0900

    ご贔屓さん出席状況画面作成（側だけ）

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,102 +1,153 @@
+// ファイル: src/app/page.tsx
+
 import Image from "next/image";
+import "./globals.css";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div>
+      <header className="header">
+        <div className="header-content">
+          <Image
+            src="/img/kotobuki_logo.jpg"
+            alt="百年蕎麦 壽 ロゴ"
+            width={80}
+            height={80}
+            className="logo"
+          />
+          {/* 日付は当日の日付を取得させる */} 
+          <h1>百年蕎麦 壽　7月10日　ご贔屓さん出席状況</h1>
+        </div>
+      </header>
+      {/* QRコードを読み込んだご贔屓さんの会員情報をDBから取得してループ表示 */}
+      <main>
+        <div className="table-scroll-area">
+          <table>
+            <thead>
+              <tr>
+                <th>出身地</th>
+                <th>ニックネーム</th>
+                <th>入り時間</th>
+                <th>出席回数</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>京都府</td>
+                <td>荷掛テスト</td>
+                <td>13:25</td>
+                <td>5</td>
+              </tr>
+              <tr>
+                <td>京都府</td>
+                <td>荷掛テスト</td>
+                <td>13:25</td>
+                <td>5</td>
+              </tr>
+              <tr>
+                <td>和歌山県</td>
+                <td>アドベンチャーワールド</td>
+                <td>14:03</td>
+                <td>107</td>
+              </tr>
+              <tr>
+                <td>沖縄県</td>
+                <td>海ぶどう</td>
+                <td>14:03</td>
+                <td>107</td>
+              </tr>
+              <tr>
+                <td>大阪府</td>
+                <td>たこ焼き命</td>
+                <td>14:03</td>
+                <td>107</td>
+              </tr>
+              <tr>
+                <td>富山県</td>
+                <td>氷見カレー</td>
+                <td>15:03</td>
+                <td>107</td>
+              </tr>
+              <tr>
+                <td>京都府</td>
+                <td>荷掛テスト</td>
+                <td>15:03</td>
+                <td>5</td>
+              </tr>
+              <tr>
+                <td>富山県</td>
+                <td>黒部ダム</td>
+                <td>15:03</td>
+                <td>107</td>
+              </tr>
+              <tr>
+                <td>北海道</td>
+                <td>いくら</td>
+                <td>15:13</td>
+                <td>107</td>
+              </tr>
+              <tr>
+                <td>和歌山県</td>
+                <td>ア</td>
+                <td>15:53</td>
+                <td>107</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      {/* 出勤スタッフの情報をDB?から取得してループ表示 */}
+      {/* 一言は当日の出勤スタッフが自由に記載できるようにする？ */}
+      <section className="staff-message-wrapper">
+        <div className="staff-section">
+          <h2>本日の出勤スタッフ</h2>
+          <table className="staff-table">
+            <thead>
+              <tr>
+                <th>出身地</th>
+                <th>スタッフ名</th>
+                <th>ニックネーム</th>
+                <th>一言</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>京都府</td>
+                <td>乾　祐樹</td>
+                <td>ぬい</td>
+                <td>店長です！</td>
+              </tr>
+              <tr>
+                <td>京都府</td>
+                <td>荷掛　聖也</td>
+                <td>にか</td>
+                <td>最近入りました！</td>
+              </tr>
+              <tr>
+                <td>京都府</td>
+                <td>馬場 喜滉</td>
+                <td>馬場ちゃん</td>
+                <td>ワインソムリエ目指してます！</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        {/* お店からの一言は当日の出勤スタッフが自由に記載できるようにする？ */}
+        <div className="message-section">
+          <h2>お店からの一言</h2>
+          <div className="message-box">
+            🍶 大阪の地酒「〇〇」入荷しました！<br />
+            🐟 本日限定「エイヒレ炙り」あります！
+          </div>
+        </div>
+      </section>
+      {/* 公式SNSのURLが変更された場合は適宜変更 */}
+      <footer>
+        <a href="https://linktr.ee/kotobuki_tachi" target="_blank">公式HP</a>
+        <a href="https://www.instagram.com/kotobuki_tachi/" target="_blank">Instagram</a>
+        <a href="https://x.com/kotobuki_tachi" target="_blank">X（旧Twitter）</a>
+        <a href="https://www.instagram.com/kotobuki_tachi/" target="_blank">Facebook（準備中）</a>
       </footer>
     </div>
   );
